4fd6945d03e894de6dd723c6dc05b978a85855fd
Elijah Brown <elibrown0731@gmail.com>
2022-10-17 11:53:48 -0400

    working on pathfinder

---
+++ pathfinder.py
@@ -0,0 +1,51 @@
+from PIL import Image 
+import os, sys
+from pyparsing import line_start
+
+matrix = np.loadtxt('matrix.txt')
+step = tf.train.matrix
+row = len(matrix)
+col = len(matrix)
+
+print(col, row)
+with file open(file, "r") as route:
+# Creating a directory containing the elevation_small coordinitaes
+# to decide which direction to walk, starting from the start
+# pick one of the 3 adjecent cells in the next column
+# if cell is coordinitaes are equal, flip a coin to pick one direction
+# elevation_change = direction
+# starting_point = direction.min()
+# def east():
+# def northeast():
+# def n
+# north_east = go_foraward_up += east
+# south_east = go_forward_down -= east
+# east = go_foraward + east
+# 
+# if elevation_change >= direction.upper()
+# direction = direction.lower()
+# else direction = east
+# direction = direction.upper()
+# if direction == direction: return random direction
+# 
+# Go through each cell in the grind
+# start = (x,y)
+# end = (x,y)
+# if
+
+
+# def scale():
+#     while True:
+#         for coordinitaes in data:
+#             if coordinitaes >+
+#     print("stuff")
+
+
+# scale()
+
+
+
+
+
+
+
